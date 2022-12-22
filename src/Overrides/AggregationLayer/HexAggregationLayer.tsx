14550ff12f227e60a80a919c34740cb2f9f1b9f4
Moritz Heckmann <moritz.heckmann@datavisyn.io>
2022-12-22 13:24:18 +0100

    sliding vis doesnt slide anymore

--- src/Overrides/AggregationLayer/HexAggregationLayer.tsx
+++ src/Overrides/AggregationLayer/HexAggregationLayer.tsx
@@ -320,7 +320,7 @@ export const HexAggregationLayer = connector(
     }, [aggregateDataset, mouseClick, setCurrentAggregateSelectionFn]);
 
     return (
-      <div>
+      <div style={{ position: 'absolute', top: '0px', left: '0px', width: '100%', height: '100%' }}>
         <LoadingIndicatorDialog
           handleClose={() => {
             cancelPromises();
