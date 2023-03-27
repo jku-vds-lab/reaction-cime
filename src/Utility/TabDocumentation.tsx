ea9cf063e1f269137c2e35998a2e2df8a0115be8
Moritz Heckmann <moritz.heckmann@datavisyn.io>
2023-03-27 13:24:27 +0200

    added documentation

--- src/Utility/TabDocumentation.tsx
+++ src/Utility/TabDocumentation.tsx
@@ -1,15 +1,36 @@
 import * as React from 'react';
-import { Box, IconButton } from '@mui/material';
+import { Box, Button, Dialog, DialogActions, DialogContent, DialogTitle, IconButton } from '@mui/material';
 import HelpIcon from '@mui/icons-material/Help';
+import ReactMarkdown from 'react-markdown';
+import readme from '../README.md?raw';
 
 export function TabDocumentation({ value }: { value: number }) {
-  const handleClick = () => {};
+  const [open, setOpen] = React.useState(false);
+
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
-    <Box sx={{ position: 'absolute', right: 8, top: 8 }}>
-      <IconButton size="small" onClick={handleClick}>
-        <HelpIcon fontSize="inherit" />
-      </IconButton>
-    </Box>
+    <>
+      <Dialog open={open} onClose={handleClose} maxWidth="xl">
+        <DialogTitle>Optional sizes</DialogTitle>
+        <DialogContent>
+          <ReactMarkdown>{readme}</ReactMarkdown>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Close</Button>
+        </DialogActions>
+      </Dialog>
+      <Box sx={{ position: 'absolute', right: 8, top: 8 }}>
+        <IconButton size="small" onClick={handleClick}>
+          <HelpIcon fontSize="inherit" />
+        </IconButton>
+      </Box>
+    </>
   );
 }
